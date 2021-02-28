3ba1ce010a2f4a9fa778fdaa996d71d8d1b419c8
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2021-02-28 20:14:42 +0700

    fix: framework

--- Calculator/ViewModel/BaseViewModel.cs
+++ Calculator/ViewModel/BaseViewModel.cs
@@ -5,7 +5,7 @@ using System.Windows.Input;
 
 namespace Calculator.ViewModel
 {
-    public class BaseViewModel: INotifyPropertyChanged
+    public class BaseViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
--- Calculator/ViewModel/CustomControlBarViewModel.cs
+++ Calculator/ViewModel/CustomControlBarViewModel.cs
@@ -2,6 +2,5 @@
 {
     public class CustomControlBarViewModel: BaseViewModel
     {
-
     }
 }
