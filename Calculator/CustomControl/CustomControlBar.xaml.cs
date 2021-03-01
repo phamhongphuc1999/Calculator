2a0d02808e4992672070f11c50edf1da2f0d65f3
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2021-03-01 11:37:06 +0700

    add: custom control bar and sub custom control bar

--- Calculator/CustomControl/CustomControlBar.xaml.cs
+++ Calculator/CustomControl/CustomControlBar.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Calculator.ViewModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calculator.CustomControl
 {
@@ -20,9 +8,13 @@ namespace Calculator.CustomControl
     /// </summary>
     public partial class CustomControlBar : UserControl
     {
+        private CustomControlBarViewModel customControlBarViewModel;
+
         public CustomControlBar()
         {
             InitializeComponent();
+            customControlBarViewModel = new CustomControlBarViewModel();
+            this.DataContext = customControlBarViewModel;
         }
     }
 }
--- Calculator/ViewModel/CustomControlBarViewModel.cs
+++ Calculator/ViewModel/CustomControlBarViewModel.cs
@@ -1,6 +1,106 @@
-﻿namespace Calculator.ViewModel
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Calculator.ViewModel
 {
-    public class CustomControlBarViewModel: BaseViewModel
+    public class CustomControlBarViewModel : BaseViewModel
     {
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand MaximizeWindowCommand { get; set; }
+        public ICommand MinimizeWindowCommand { get; set; }
+        public ICommand MouseLeftButtonDownCommand { get; set; }
+
+        private string maximizeIcon;
+        public string MaximizeIcon
+        {
+            get { return maximizeIcon; }
+            set
+            {
+                maximizeIcon = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public CustomControlBarViewModel()
+        {
+            MaximizeIcon = "WindowMaximize";
+
+            InitializeCloseWindowCommand();
+            InitializeMaximizeWindowCommand();
+            InitializeMinimizeWindowCommand();
+            InitializeMouseLeftButtonDownCommand();
+        }
+
+        private FrameworkElement GetRootParent(UserControl control)
+        {
+            FrameworkElement parent = control;
+            while (parent.Parent != null)
+                parent = parent.Parent as FrameworkElement;
+            return parent;
+        }
+
+        private void InitializeCloseWindowCommand()
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>(
+                sender =>
+                {
+                    return sender != null;
+                }, sender =>
+                {
+                    FrameworkElement parent = GetRootParent(sender);
+                    Window rootWindow = parent as Window;
+                    if (rootWindow != null) rootWindow.Close();
+                });
+        }
+
+        private void InitializeMaximizeWindowCommand()
+        {
+            MaximizeWindowCommand = new RelayCommand<UserControl>(
+                sender =>
+                {
+                    return sender != null;
+                }, sender =>
+                {
+                    MaximizeIcon = maximizeIcon == "WindowMaximize" ? "WindowRestore" : "WindowMaximize";
+                    FrameworkElement root = GetRootParent(sender);
+                    Window window = root as Window;
+                    if (window != null)
+                    {
+                        if (window.WindowState != WindowState.Maximized) window.WindowState = WindowState.Maximized;
+                        else window.WindowState = WindowState.Normal;
+                    }
+                });
+        }
+
+        private void InitializeMinimizeWindowCommand()
+        {
+            MinimizeWindowCommand = new RelayCommand<UserControl>(
+                sender =>
+                {
+                    return sender == null ? false : true;
+                },
+                sender =>
+                {
+                    FrameworkElement root = GetRootParent(sender);
+                    Window window = root as Window;
+                    if (window != null) window.WindowState = WindowState.Minimized;
+                });
+        }
+
+        private void InitializeMouseLeftButtonDownCommand()
+        {
+            MouseLeftButtonDownCommand = new RelayCommand<UserControl>(
+                sender =>
+                {
+                    return sender == null ? false : true;
+                },
+                sender =>
+                {
+                    FrameworkElement root = GetRootParent(sender);
+                    Window window = root as Window;
+                    if (window != null) window.DragMove();
+                });
+        }
     }
 }
